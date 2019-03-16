feb13a245ad5c80735e51bf2820c29d12be6f6f3
willmcfall <wmcfall2@gmail.com>
2019-03-16 00:14:30 -0400

    fourth commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,63 @@
-//
-//
+// Create variable for random generated number to be selected
+var targetNumber= 0;
+console.log(targetNumber)
+
+// Create variable for total score of combined crystal valyes
+var crystalNumber= 0;
+console.log(crystalNumber)
+
+
+// Create variable for number of wins achieved by the user
+var winNumber= 0;
+console.log(winNumber)
+
+
+// Create variable for number of losses achieved by the user
+var winLoss= 0;
+console.log(losslNumber)
+
+
+// method that waits for the HTML page to load then runs a function
+$( document ).ready(function() {
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
