0f94c82eefba244f355f4ed10254a515ab9e5295
willmcfall <wmcfall2@gmail.com>
2019-03-18 15:45:08 -0400

    fifth commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,107 +1,77 @@
 
-// Create variable for random generated number to be selected
-var targetNumber = 0;
-console.log(targetNumber);
-
-// Create variable for total score of combined crystal valyes
-var crystalNumber = 0;
-console.log(crystalNumber);
-
-
-// Create variable for number of wins achieved by the user
-var winNumber = 0;
-console.log(winNumber);
-
-
-// Create variable for number of losses achieved by the user
-var lossNumber = 0;
-console.log(lossNumber);
 
 // method that waits for the HTML page to load then runs a function
 $(document).ready(function () {
 
 
+  // Create variable for number of wins achieved by the user
+  var winNumber = 0;
+  console.log(winNumber);
+
+
+  // Create variable for number of losses achieved by the user
+  var lossNumber = 0;
+  console.log(lossNumber);
+
 
   // method that waits for the start button to be clicked
-  $("#start_button").click(function () {
+  $("#start_button").on("click", function () {
 
-    console.log("Click button worked");
+    console.log("Start button worked");
 
-    // function to display number of wins and losses and draw random target number
-    function roundIteration() {
-
-      $("#num_wins").html("Number of Wins: " + winNumber)
-
-      $("#num_loses").html("Number of Loses: " + lossNumber)
-
-      targetNumber = Math.floor(Math.random() * 120) + 19;
-
-      $("#target_num").html("Number of Points Targeted: " + targetNumber)
-
-      // function to display number of crystal points and draw random crytal numbers
-      function clickIteration() {
-
-        // If statement to check for wins and losses
-        if (crystalNumber < targetNumber) {
-
-          var crystalOne = 0;
-          var crystalTwo = 0;
-          var crystalThree = 0;
-          var crystalFour = 0;
+    // Create variable for random generated number to be selected
+    var targetNumber = 0;
+    console.log(targetNumber);
 
 
-          crystalOne = Math.floor(Math.random() * 12) + 1;
-          console.log(crystalOne);
-          crystalTwo = Math.floor(Math.random() * 12) + 1;
-          console.log(crystalTwo);
-          crystalThree = Math.floor(Math.random() * 12) + 1;
-          console.log(crystalThree);
-          crystalFour = Math.floor(Math.random() * 12) + 1;
-          console.log(crystalFour);
+    // Create variable lock game
+    var lockGame = false;
+    console.log(lockGame);
 
-          $("#gem_1").click(function () {
-            crystalNumber = crystalNumber + crystalOne;
-            $("#crystal_num").html("Number of Crystal Points: " + crystalNumber);
-          });
 
-          $("#gem_2").click(function () {
-            crystalNumber = crystalNumber + crystalTwo;
-            $("#crystal_num").html("Number of Crystal Points: " + crystalNumber);
-          });
+    targetNumber = Math.floor(Math.random() * 120) + 19;
 
-          $("#gem_3").click(function () {
-            crystalNumber = crystalNumber + crystalThree;
-            $("#crystal_num").html("Number of Crystal Points: " + crystalNumber);
-          });
+    $("#target_num").html("Number of Points Targeted: " + targetNumber)
 
-          $("#gem_4").click(function () {
-            crystalNumber = crystalNumber + crystalFour;
-            $("#crystal_num").html("Number of Crystal Points: " + crystalNumber);
-          });
+    $("#gem_1").val(Math.floor(Math.random() * 12) + 1);
+    $("#gem_2").val(Math.floor(Math.random() * 12) + 1);
+    $("#gem_3").val(Math.floor(Math.random() * 12) + 1);
+    $("#gem_4").val(Math.floor(Math.random() * 12) + 1);
 
-        }
+    var crystalNumber = 0;
+    console.log(crystalNumber);
 
-        else if (crystalNumber === targetNumber) {
-          winNumber++;
-          alert("You won!");
-          var crystalNumber = 0;
-        }
+    // Here we create the on click event that gets the user"s pick
+    $(".gem").on("click", function () {
 
-        else {
-          lossNumber++;
-          alert("You lost!");
-          var crystalNumber = 0;
-        };
+      // Here this lockGame line prevents the user from changing the option after the game is done.
+      if (lockGame !== true) {
 
+
+        // We get the value associated with the button the user picked from here
+        var yourPick = $(this).val();
+        console.log("Your Pick: " + yourPick);
+        crystalNumber += parseInt(yourPick);
+
+        $("#crystal_num").html("Number of Crystal Points: " + crystalNumber);
+
+      }
+
+      if (crystalNumber == targetNumber) {
+        winNumber++;
+        $("#num_wins").html("Number of Wins: " + winNumber);
+        lockGame = true;
+        crystalNumber = 0;
+
+      } else if (crystalNumber > targetNumber) {
+        lossNumber++;
+        $("#num_loses").html("Number of Loses: " + lossNumber);
+        lockGame = true;
+        crystalNumber = 0;
       };
 
-      clickIteration();
-
-    };
-
-    roundIteration();
+    });
 
   });
-
 
 });
